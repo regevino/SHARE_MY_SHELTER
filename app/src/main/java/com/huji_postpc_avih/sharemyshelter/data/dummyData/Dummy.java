afe95ee27085030cdb9353b0d253e04128eaf2ec
Avinoam <avinoam@hershler.com>
2021-09-23 10:53:29 +0300

    Add dummy shelter service

---
+++ app/src/main/java/com/huji_postpc_avih/sharemyshelter/data/dummyData/Dummy.java
@@ -0,0 +1,15 @@
+package com.huji_postpc_avih.sharemyshelter.data.dummyData;
+
+import android.content.Context;
+import android.location.Location;
+
+import com.huji_postpc_avih.sharemyshelter.data.Shelter;
+
+public class Dummy {
+
+    public static Shelter getDummyShelter(Context c)
+    {
+        return new Shelter(c, new Location(""), "Dummy shelter", Shelter.ShelterType.PUBLIC, null);
+    }
+
+}
