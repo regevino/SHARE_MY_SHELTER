e1425d69d62b4111870693904fb4c9a6b6d7620f
Idan <idan.stollman@mail.huji.ac.il>
2021-09-26 12:19:20 +0300

    AddShelterActivity divided into two fragments.

---
+++ app/src/main/java/com/huji_postpc_avih/sharemyshelter/ui/dashboard/add_shelter/AddShelterActivity.java
@@ -0,0 +1,39 @@
+package com.huji_postpc_avih.sharemyshelter.ui.dashboard.add_shelter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.huji_postpc_avih.sharemyshelter.R;
+import com.huji_postpc_avih.sharemyshelter.SheltersApp;
+import com.huji_postpc_avih.sharemyshelter.data.Shelter;
+import com.huji_postpc_avih.sharemyshelter.data.ShelterDB;
+import com.huji_postpc_avih.sharemyshelter.navigation.Navigator;
+import com.vansuita.pickimage.bean.PickResult;
+import com.vansuita.pickimage.bundle.PickSetup;
+import com.vansuita.pickimage.dialog.PickImageDialog;
+import com.vansuita.pickimage.listeners.IPickResult;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AddShelterActivity extends AppCompatActivity{
+
+}
