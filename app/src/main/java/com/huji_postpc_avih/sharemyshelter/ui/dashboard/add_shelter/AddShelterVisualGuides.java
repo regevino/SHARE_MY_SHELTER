8dceb9290733096d5ea8ca2d9b8253ea19235a51
Avinoam <avinoam@hershler.com>
2021-09-26 14:21:13 +0300

    Remove params from AddShelterVisualGuides c'tor

--- app/src/main/java/com/huji_postpc_avih/sharemyshelter/ui/dashboard/add_shelter/AddShelterVisualGuides.java
+++ app/src/main/java/com/huji_postpc_avih/sharemyshelter/ui/dashboard/add_shelter/AddShelterVisualGuides.java
@@ -35,13 +35,10 @@ public class AddShelterVisualGuides extends Fragment implements IPickResult {
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment AddShelterVisualGuides.
      */
     // TODO: Rename and change types and number of parameters
-    public static AddShelterVisualGuides newInstance(String param1, String param2) {
+    public static AddShelterVisualGuides newInstance() {
         AddShelterVisualGuides fragment = new AddShelterVisualGuides();
         Bundle args = new Bundle();
         fragment.setArguments(args);
