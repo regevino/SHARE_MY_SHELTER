97eadc3b5c785c953d72ab60957d6c51f48543e3
Avinoam <avinoam@hershler.com>
2021-09-28 23:51:38 +0300

    Add settings screen

--- app/src/main/java/com/huji_postpc_avih/sharemyshelter/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/huji_postpc_avih/sharemyshelter/ui/notifications/NotificationsFragment.java
@@ -1,46 +1,45 @@
 package com.huji_postpc_avih.sharemyshelter.ui.notifications;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.huji_postpc_avih.sharemyshelter.R;
-import com.huji_postpc_avih.sharemyshelter.databinding.FragmentNotificationsBinding;
+import com.huji_postpc_avih.sharemyshelter.SheltersApp;
+import com.huji_postpc_avih.sharemyshelter.users.UserManagerFirebase;
 
-public class NotificationsFragment extends Fragment {
+import androidx.preference.PreferenceFragmentCompat;
 
-    private NotificationsViewModel notificationsViewModel;
-    private FragmentNotificationsBinding binding;
+public class NotificationsFragment extends PreferenceFragmentCompat {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+        updateSignInInfo();
+        getPreferenceManager().findPreference("preference_signout").setOnPreferenceClickListener(preference ->
+                {
+                    ((SheltersApp)(getActivity().getApplication())).getUserManager().signOut();
+                    updateSignInInfo();
+                    return false;
+                }
+        );
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onResume() {
+        super.onResume();
+        updateSignInInfo();
+    }
+
+    private void updateSignInInfo() {
+        UserManagerFirebase userManager = ((SheltersApp) (getActivity().getApplication())).getUserManager();
+        if (userManager.getCurrentUser() != null) {
+            getPreferenceManager().findPreference("preference_signout").setVisible(true);
+            getPreferenceManager().findPreference("preference_signin").setVisible(false);
+            getPreferenceManager().findPreference("preference_account").setSummary("Signed in as " + userManager.authenticatedUser.getValue().getDisplayName());
+        } else {
+            getPreferenceManager().findPreference("preference_signout").setVisible(false);
+            getPreferenceManager().findPreference("preference_signin").setVisible(true);
+            getPreferenceManager().findPreference("preference_account").setSummary("Signed out");
+        }
     }
 }
